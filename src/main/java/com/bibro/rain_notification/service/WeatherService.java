e504104c41df3174faab9bf893d0ae30fa8261fa
mikolajbibro <mikolajbibro@gmail.com>
2019-11-04 16:57:23 +0100

    new attribute LocationRepository and changes in setLastRainyWeatherForUserLocation(Location)

--- src/main/java/com/bibro/rain_notification/service/WeatherService.java
+++ src/main/java/com/bibro/rain_notification/service/WeatherService.java
@@ -4,6 +4,7 @@ import com.bibro.rain_notification.domain.user.AppUser;
 import com.bibro.rain_notification.domain.user.Location;
 import com.bibro.rain_notification.domain.weather.Weather;
 import com.bibro.rain_notification.domain.weather.WeatherApi;
+import com.bibro.rain_notification.repository.LocationRepository;
 import com.bibro.rain_notification.repository.UserRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,7 @@ public class WeatherService {
     private WeatherApi weatherApi;
     private EmailService emailService;
     private UserService userService;
+    private LocationRepository locationRepository;
 
     public WeatherService(WeatherApi weatherApi, EmailService emailService, UserService userService) {
         this.weatherApi = weatherApi;
@@ -28,17 +30,20 @@ public class WeatherService {
     private void notifyByLocation(Location location, AppUser user) throws IOException {
         Weather weather = weatherApi.getWeather(location);
 
-        if (shouldSendNotification(location, user)) {
+        if (shouldSendNotification(location)) {
             emailService.sendNotification(user, weather);
         }
     }
 
-    private void setLastRainyWeatherForUserLocation(Location location, AppUser user) {
-        if (weatherApi.getWeather(location).isRainy()) userService.setLastRainyWeatherForNow(user);
+    private void setLastRainyWeatherForUserLocation(Location location) {
+        if (weatherApi.getWeather(location).isRainy()) {
+            location.setLastRainyWeather(LocalDateTime.now());
+            locationRepository.save(location);
+        }
     }
 
-    private boolean shouldSendNotification(Location location, AppUser user) {
-        return weatherApi.getWeather(location).isRainy() && (user.getLastRainyWeather() == null || user.getLastRainyWeather().isBefore(LocalDateTime.now().minusHours(1)));
+    private boolean shouldSendNotification(Location location) {
+        return weatherApi.getWeather(location).isRainy() && (location.getLastRainyWeather() == null || location.getLastRainyWeather().isBefore(LocalDateTime.now().minusHours(1)));
     }
 
     @Scheduled(fixedDelay = 15 * 60 * 1000)
@@ -49,7 +54,7 @@ public class WeatherService {
                 .forEach(location -> {
                     try {
                         notifyByLocation(location, appUser);
-                        setLastRainyWeatherForUserLocation(location, appUser);
+                        setLastRainyWeatherForUserLocation(location);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
